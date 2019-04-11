35ca5f5032fae5d9aa26e5d32824c7a35fcf70df
ArkishaP <patelarkisha@gmail.com>
2019-04-11 10:31:04 +0530

    Page not found added

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use("/api/todo", todo);
 
 // Page not found
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname, './client/public/index.html'));
   });
 
 app.use(express.static(path.join(__dirname,"client/build")))
